06bd40e385fe720a69cc2ed1d0b22224568fc7d7
switer <guankaishe@gmail.com>
2015-02-05 00:13:45 +0800

    skeleton compelete

--- routed.js
+++ routed.js
@@ -5,9 +5,29 @@
     var evt = 'hashchange'
     var lts = []
 
+    /**
+     *  e.g: http://github.com#profile ==> #profile 
+     */
+    function hashFromUrl(url) {
+        return (url || '').split('#')[0]
+    }
 
+    function emit (ctx) {
+        for (var i = 0; i < lts.length; i++) {
+            lts(ctx)
+        }
+    }
+
+    /**
+     *  call when hashchange trigger
+     */
     function dispatcher(e) {
+        var pre = hashFromUrl(e.oldURL)
+        var next = hashFromUrl(e.newURL)
+        var ctx = ctxes.pop()
 
+        // validate
+        // if (ctx && ctx.pre === pre) {}
     }
 
     function Context(opt) {
@@ -19,13 +39,25 @@
     }
 
     R.back = function() {
-
+        var ctx = new Context({
+            type: 'back',
+            pre: window.location.hash.slice(1)
+        })
+        ctxes.push(ctx)
     }
     R.forward = function() {
-
+        var ctx = new Context({
+            type: 'forward',
+            pre: window.location.hash.slice(1)
+        })
+        ctxes.push(ctx)
     }
     R.route = function() {
-
+        var ctx = new Context({
+            type: 'route',
+            pre: window.location.hash.slice(1)
+        })
+        ctxes.push(ctx)
     }
 
     /**
@@ -43,9 +75,7 @@
 
     // AMD/CMD/node/bang
     if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && module.exports) exports = module.exports = R
-
-        exports.Routed = R
+        if (typeof module !== 'undefined' && module.exports) module.exports = R
+        else exports.Routed = R
     } else this.Routed = R
-
 }();
